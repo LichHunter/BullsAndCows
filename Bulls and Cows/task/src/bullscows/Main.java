57846de4b129838aa45900f4eb7d65a3def06945
AlexD2019 <alexander0derevianko@gmail.com>
2020-12-28 11:20:22 +0000

    Completed first stage

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,8 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("The secret code is prepared: ****");
+        System.out.println("Turn 1. Answer:\n1234\nGrade: None.");
+        System.out.println("Turn 2. Answer:\n9876\nGrade: 4 bulls.\nCongrats! The secret code is 9876");
     }
 }

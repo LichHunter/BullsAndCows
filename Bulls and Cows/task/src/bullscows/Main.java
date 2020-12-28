cbf0537c9fd8928dd276cad6fea93ce0f72886af
AlexD2019 <alexander0derevianko@gmail.com>
2020-12-28 14:48:50 +0000

    Completed third task

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -9,20 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String number = generateNumber();
-        String userGuess;
-        do {
-            userGuess = scanner.next();
-        } while (!isCorrectInput(userGuess));
-
-        compare(number, userGuess);
-
-        if (BULL == 0 && COW == 0) {
-            System.out.println("Grade: None");
-        } else {
-            System.out.format("Grade: %d bull(s) and %d cow(s)\n", BULL, COW);
-        }
-        System.out.println("The secret code is " + number);
+        String number = generateNumber(scanner.nextInt());
+        System.out.println("The random secret number is " + number);
+//        String userGuess;
+//        do {
+//            userGuess = scanner.next();
+//        } while (!isCorrectInput(userGuess));
+//
+//        compare(number, userGuess);
+//
+//        if (BULL == 0 && COW == 0) {
+//            System.out.println("Grade: None");
+//        } else {
+//            System.out.format("Grade: %d bull(s) and %d cow(s)\n", BULL, COW);
+//        }
+//        System.out.println("The secret code is " + number);
     }
 
     private static void compare(String number, String userGuess) {
@@ -43,7 +44,41 @@ public class Main {
         return Integer.parseInt(num) >= 1000 && Integer.parseInt(num) <= 9999;
     }
 
-    private static String generateNumber() {
-        return String.valueOf(new Random().nextInt(8999) + 1000);
+    private static String generateNumber(int length) {
+        if (length > 10) {
+            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            return "";
+        } else {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+
+            while (builder.length() != length) {
+                if (builder.length() != 0) {
+                    int num;
+                    do {
+                        num = random.nextInt(10);
+                    } while (digitIsInArray(builder.toString().toCharArray(), num));
+
+                    builder.append(num);
+                } else {
+                    int num;
+                    do {
+                        num = random.nextInt(10);
+                    } while (num == 0);
+
+                    builder.append(num);
+                }
+            }
+
+            return builder.toString();
+        }
+    }
+
+    private static boolean digitIsInArray(char[] digits, int num) {
+        for (char digit : digits) {
+            if (String.valueOf(digit).equals(String.valueOf(num))) return true;
+        }
+
+        return false;
     }
 }

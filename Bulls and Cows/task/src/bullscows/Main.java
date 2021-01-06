bdc03df0d35efc603892948f8876e2ef8604db28
AlexD2019 <alexander0derevianko@gmail.com>
2021-01-06 11:41:18 +0000

    Completed seventh stage

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -5,100 +5,121 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please, enter the secret code's length: ");
-        int codeLength = scanner.nextInt();
-        System.out.println("Please, enter the number of possible symbols in the code: ");
-        int rangeOfCharacters = scanner.nextInt();
-        char[] charLibrary = createLibrary(rangeOfCharacters);
-        String code = generateCode(codeLength, charLibrary);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int codeLength = 0;
+		int rangeOfCharacters = 0;
 
-        System.out.print("The secret code is prepared: ");
-        for (char c : code.toCharArray()) {
-            System.out.print("*");
-        }
-        System.out.print("(0-");
-        if (rangeOfCharacters < 10) {
-            System.out.print(charLibrary[charLibrary.length - 1] + ").");
-        } else if (rangeOfCharacters == 10) {
-            System.out.print("9).");
-        } else {
-            System.out.print("9, a-" + charLibrary[charLibrary.length - 1] + ").");
-        }
-        System.out.println();
-        System.out.println("Okay, let's start a game!");
+		try {
+			System.out.println("Please, enter the secret code's length: ");
+			codeLength = scanner.nextInt();
+			System.out.println("Please, enter the number of possible symbols in the code: ");
+			rangeOfCharacters = scanner.nextInt();
 
-        int turn = 1;
-        while (true) {
-            int bull = 0;
-            int cow = 0;
+			if (codeLength > rangeOfCharacters) {
+				System.out.format("Error: it's not possible to generate a code with a length of %d with %d unique symbols.\n"
+						, codeLength, rangeOfCharacters);
+				System.exit(1);
+			} else if (codeLength == 0 || rangeOfCharacters == 0) {
+				System.out.println("Error: code length and number of possible symbols must be greater than 0");
+				System.exit(1);
+			} else if (rangeOfCharacters > 36) {
+				System.out.format("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", rangeOfCharacters);
+				System.exit(1);
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getCause() + "isn't a valid number");
+			System.exit(1);
+		}
 
-            System.out.println("Turn " + turn);
-            String userGuess = scanner.next();
+		char[] charLibrary = createLibrary(rangeOfCharacters);
+		String code = generateCode(codeLength, charLibrary);
 
-            for (int i = 0; i < code.length(); i++) {
-                for (int j = 0; j < userGuess.length(); j++) {
-                    if (code.charAt(i) == userGuess.charAt(j)) {
-                        if (i == j) {
-                            bull++;
-                        } else {
-                            cow++;
-                        }
-                    }
-                }
-            }
+		System.out.print("The secret code is prepared: ");
+		for (char c : code.toCharArray()) {
+			System.out.print("*");
+		}
+		System.out.print("(0-");
+		if (rangeOfCharacters < 10) {
+			System.out.print(charLibrary[charLibrary.length - 1] + ").");
+		} else if (rangeOfCharacters == 10) {
+			System.out.print("9).");
+		} else {
+			System.out.print("9, a-" + charLibrary[charLibrary.length - 1] + ").");
+		}
+		System.out.println();
+		System.out.println("Okay, let's start a game!");
 
-            if (code.equals(userGuess)) {
-                System.out.format("Grade: %d bull(s) and %d cow(s)\n", bull, cow);
-                System.out.println("Congratulations! You guessed the secret code.");
-                break;
-            } else if (bull == 0 && cow == 0) {
-                System.out.println("Grade: None");
-            } else {
-                System.out.format("Grade: %d bull(s) and %d cow(s)\n", bull, cow);
-            }
+		int turn = 1;
+		while (true) {
+			int bull = 0;
+			int cow = 0;
 
-            turn++;
-        }
-    }
+			System.out.println("Turn " + turn);
+			String userGuess = scanner.next();
 
-    private static String generateCode(int length, char[] chars) {
-        if (length > 36) {
-            System.out.format("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", length);
-            return "";
-        } else {
-            StringBuilder builder = new StringBuilder();
-            Random random = new Random();
+			if (userGuess.length() > code.length() || userGuess.length() < code.length()) {
+				System.out.println("Error: answer can't be shorter or bigger than code.");
+				System.exit(1);
+			}
 
-            while (builder.length() != length) {
-                char c;
-                do {
-                    c = chars[random.nextInt(chars.length)];
-                } while (isCharInCode(builder.toString().toCharArray(), c));
+			for (int i = 0; i < code.length(); i++) {
+				for (int j = 0; j < userGuess.length(); j++) {
+					if (code.charAt(i) == userGuess.charAt(j)) {
+						if (i == j) {
+							bull++;
+						} else {
+							cow++;
+						}
+					}
+				}
+			}
 
-                builder.append(c);
-            }
+			if (code.equals(userGuess)) {
+				System.out.format("Grade: %d bull(s) and %d cow(s)\n", bull, cow);
+				System.out.println("Congratulations! You guessed the secret code.");
+				break;
+			} else if (bull == 0 && cow == 0) {
+				System.out.println("Grade: None");
+			} else {
+				System.out.format("Grade: %d bull(s) and %d cow(s)\n", bull, cow);
+			}
 
-            return builder.toString();
-        }
-    }
+			turn++;
+		}
+	}
 
-    private static boolean isCharInCode(char[] chars, char c) {
-        for (char ch : chars) {
-            if (ch == c) return true;
-        }
+	private static String generateCode(int length, char[] chars) {
+		StringBuilder builder = new StringBuilder();
+		Random random = new Random();
 
-        return false;
-    }
+		while (builder.length() != length) {
+			char c;
+			do {
+				c = chars[random.nextInt(chars.length)];
+			} while (isCharInCode(builder.toString().toCharArray(), c));
 
-    private static char[] createLibrary(int size) {
-        char[] chars = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'q', 'w', 'e', 'r', 't',
-                'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-        Arrays.sort(chars);
-        char[] result = new char[size];
-        if (size >= 0) System.arraycopy(chars, 0, result, 0, size);
+			builder.append(c);
+		}
 
-        return result;
-    }
+		return builder.toString();
+	}
+
+	private static boolean isCharInCode(char[] chars, char c) {
+		for (char ch : chars) {
+			if (ch == c) return true;
+		}
+
+		return false;
+	}
+
+	private static char[] createLibrary(int size) {
+		char[] chars = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'q', 'w', 'e', 'r', 't',
+				'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+		Arrays.sort(chars);
+		char[] result = new char[size];
+		if (size >= 0) System.arraycopy(chars, 0, result, 0, size);
+
+		return result;
+	}
 }
